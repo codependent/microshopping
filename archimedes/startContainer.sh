d30ac44f5950eedd2bb4bad604cb5e2a9b55b3fb
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2017-03-06 09:08:08 +0100

    kubernetes order and product services

--- archimedes/startContainer.sh
+++
@@ -1,5 +0,0 @@
-docker run -e "SPRING_PROFILES_ACTIVE=archimedes1" -p 8761:8761 \
-  --name archimedes1 --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host my-rabbit:192.168.99.100 \
-  -d storyteller/archimedes 
-docker run -e "SPRING_PROFILES_ACTIVE=archimedes2" -p 8762:8762 \
-  --name archimedes2 --add-host archimedes1:192.168.99.100 --add-host archimedes2:192.168.99.100 --add-host my-rabbit:192.168.99.100 -d storyteller/archimedes
--- order-service/src/main/java/com/codependent/microshopping/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/codependent/microshopping/order/OrderServiceApplication.java
@@ -3,7 +3,6 @@ package com.codependent.microshopping.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,7 +13,6 @@ import com.codependent.microshopping.order.stream.OrderProcessor;
 import com.codependent.stream.DelegatedMessagingConfig;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableJpaRepositories(basePackageClasses=OrderDao.class)
 @EntityScan(basePackageClasses=OrderEntity.class)
 @EnableBinding(OrderProcessor.class)
